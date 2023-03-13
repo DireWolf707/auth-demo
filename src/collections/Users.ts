19045ec26e9fdff55701566574fd3123a6c76963
DireWolf707 <rahulsood707@gmail.com>
2023-03-13 17:34:54 +0530

    modified email sent

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -7,10 +7,37 @@ const Users: CollectionConfig = {
     cookies: {
       secure: true,
       sameSite: "none",
-      // domain: "???",
+      // domain: process.env.PAYLOAD_PUBLIC_FRONTEND_URL,
     },
     // verify: true, // { //email gen and subject gen fxns }
-    // forgotPassword: { //email gen and subject gen fxns },
+    forgotPassword: {
+      generateEmailHTML: ({ req, token, user }) => {
+        const resetPasswordURL = `${process.env.PAYLOAD_PUBLIC_FRONTEND_URL}/reset-password/${token}`
+
+        return `
+        <!doctype html>
+        <html>
+          <body>
+            <h1>
+              Hello, ${user.name}!
+            </h1>
+            <p>
+              You are receiving this because you (or someone else) have 
+              requested the reset of the password for your account. 
+            </p>
+            <p>Click below to reset your password.</p>
+            <p>
+              <a href="${resetPasswordURL}">${resetPasswordURL}</a>
+            </p>
+            <p>
+              If you did not request this, please ignore this email and your password will 
+              remain unchanged.
+            </p>
+          </body>
+        </html>
+      `
+      },
+    },
     // strategies: [{strategy: (ctx)=>ctx.au,}],
   },
   admin: {
